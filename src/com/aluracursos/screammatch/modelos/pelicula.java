e33005cfd709ea09bc04cf5eb7f6f69711725104
Santiago Macias <smaciasr@unal.edu.co>
2024-04-13 20:29:30 -0500

    Agregando archivos iniciales al repositorio"

---
+++ src/com/aluracursos/screammatch/modelos/pelicula.java
@@ -0,0 +1,23 @@
+public class pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+    int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+    void muestraFichaTecnica(){
+        System.out.println("El nombre de la pelicula es:"+ nombre);
+        System.out.println("Su fecha de lanzamiento es: "+ fechaDeLanzamiento);
+        System.out.println("La duración de la pelicula es:" + duracionEnMinutos);
+    }
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
